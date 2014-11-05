66e2050a3cd5ad1adc46294c9e56bdea76601560
SZR <szymon.rut@agileo.it>
2014-11-05 23:40:47 +0100

    In Exchange: 1) Required open-uri 2) Added currencies association 3) Added name validations 4) Added before validation callback 5) Added bodies of get_nbp_xml and save_current_rates methods 6) Added xml_list and xml_name methods

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,11 +1,31 @@
+require "open-uri"
+
 class Exchange < ActiveRecord::Base
-  #you can change names if you don't like them
+  has_many :currencies, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+
+  before_validation { self.name = xml_name }
 
   def get_nbp_xml
-
+    @get_nbp_xml ||= open("http://www.nbp.pl/kursy/xml/#{xml_name}.xml").read
   end
 
   def save_current_rates
+    self.save
+    rates = Hash.from_xml(get_nbp_xml)["tabela_kursow"]["pozycja"].map do |rate|
+      Currency.new(name: rate["nazwa_waluty"], converter: rate["przelicznik"], code: rate["kod_waluty"],
+        buy_price: rate["kurs_kupna"], sell_price: rate["kurs_sprzedazy"], exchange: self)
+    end
+    Currency.import rates
+  end
 
+  private
+  def xml_list
+    open("http://nbp.pl/kursy/xml/dir.txt").read
+  end
+
+  def xml_name
+    xml_list.lines.select{ |l| l.match(/c([0-9]+)z#{Date.today.strftime "%y%m%d"}/) }.first[0..-3] rescue nil
   end
 end
--- spec/models/exchange_spec.rb
+++ spec/models/exchange_spec.rb
@@ -1,5 +1,87 @@
 require 'spec_helper'
 
 describe Exchange do
-  pending "add some examples to (or delete) #{__FILE__}"
+  EXCHANGE_COLUMNS = %w[name created_at updated_at]
+
+  describe "via relations" do
+    it { should have_many(:currencies).dependent(:destroy) }
+  end
+
+  describe "via validations" do
+    before(:each) do
+      @exchange = FactoryGirl.build(:exchange)
+    end
+
+    it { @exchange.should be_valid }
+
+    it "should validate presence of name" do
+      Timecop.freeze(Date.tomorrow) do
+        @exchange.should_not be_valid
+        @exchange.errors.messages.should eq({ name: ["can't be blank"] })
+      end
+    end
+
+    it "should validate uniqueness of name - can't create 2 exchanges the same day" do
+      @exchange.save
+      exchange2 = FactoryGirl.build(:exchange)
+      exchange2.should_not be_valid
+      exchange2.errors.messages.should eq({ name: ["has already been taken"] })
+    end
+  end
+
+  describe "via callbacks" do
+    before(:each) do
+      @exchange = FactoryGirl.build(:exchange)
+      Timecop.freeze(Date.today.beginning_of_week + 10.hours)
+    end
+
+    after(:each){ Timecop.return }
+
+    context "before validation" do
+      it "should set xml's name as own name" do
+        @exchange.name.should eq(nil)
+        @exchange.valid?
+        @exchange.name.should eq(@exchange.send(:xml_name))
+      end
+    end
+  end
+
+  describe "via instance methods" do
+    before(:each) do
+      @exchange = FactoryGirl.build(:exchange)
+      Timecop.freeze(Date.today.beginning_of_week + 10.hours)
+    end
+
+    after(:each){ Timecop.return }
+
+    context "#get_nbp_xml" do
+      it "should return xml with rates for today" do
+        @exchange.get_nbp_xml.strip[/\A<\?xml/].should_not be_nil
+        Hash.from_xml(@exchange.get_nbp_xml)["tabela_kursow"]["data_publikacji"].should eq(Date.today.to_s)
+      end
+
+      it "should raise error if xml for today doesn't exist" do
+        Timecop.freeze(Date.today.beginning_of_week - 1.day) do
+          expect { @exchange.get_nbp_xml }.to raise_error(OpenURI::HTTPError, "404 Not Found")
+        end
+      end
+    end
+
+    context "#save_current_rates" do
+      it "should save self rates for every currency" do
+        @exchange.save_current_rates
+        @exchange.persisted?.should eq(true)
+        @exchange.currencies.size.should eq(13)
+        @exchange.currencies.all?(&:valid?).should eq(true)
+      end
+    end
+  end
+
+  describe "DB columns" do
+    EXCHANGE_COLUMNS.each do |c|
+      it "should include '#{c}'" do
+        Exchange.column_names.should include(c)
+      end
+    end
+  end
 end
